db234f9cfc2881c5b2deb212e232826145294b1c
Shailendra Kushwaha <learning4succedding@gmail.com>
2019-10-17 09:34:39 +0300

    added changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class FetchGitHub extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      keyword: "",
+      data: [] //As repositories are returned as JSON arrays in response.
+    };
+  }
+
+  //Adding change listener to input field to be able to save the input value to the state, called keyword.
+  handleChange = event => {
+    this.setState({ keyword: event.target.value });
+  };
+  //The function does REST API call when  it it invoked by pressing button.
+  fetchData = () => {
+    const url = `https://api.github.com/search/repositories?q=${this.state.keyword}`;
+    fetch(url)
+      .then(response => response.json())
+      .then(responseData => {
+        this.setState({
+          data: responseData.items
+        });
+      });
+  };
+
+  render() {
+    const itemRows = this.state.data.map((item, index) => (
+      <tr key={index}>
+        <td>{item.full_name}</td>
+        <td>
+          <a href={item.html_url}>{item.html_url}</a>
+        </td>
+      </tr>
+    ));
+    return (
+      <div className="App">
+        <h1>Repositories</h1>
+        <input type="text" onChange={this.handleChange} />
+        <button onClick={this.fetchData} value={this.state.keyword}>
+          Fetch
+        </button>
+        <table>
+          <tbody>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>URL</th>
+              </tr>
+            </thead>
+            {itemRows}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default FetchGitHub;
